fedcf2678e7b7ff287cb8145cb522d3539c54565
harshitkalra03 <harshit.kalra03@gmail.com>
2025-03-31 17:01:44 +0530

    Removed the unwanted files.

--- slicing_list.py
+++
@@ -1,6 +0,0 @@
-piano_keys = ["a", "b", "c", "d", "e", "f", "g"]
-print(piano_keys[::2])
-# very nice trick for reversing the list.
-print(piano_keys[::-1])
-
-# Slicing also works very well for tuples....
